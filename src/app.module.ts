65d40bd4e348a5fb18702de8ce6151f4ea0dec25
Aneeb-Oni <aneebdev77@gmail.com>
2025-02-28 02:26:56 +0500

    implement swagger -environment make dynamic

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,32 +14,60 @@ import { PlayListModule } from './playlists/playlists.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
-import { dataSourceOptions } from 'db/data-source';
+import { dataSourceOptions, typeOrmAsyncConfig } from 'db/data-source';
 import { SeedModule } from './seed/seed.module';
+import {envSchema} from "./config/schema/env.schema";
+import {ConfigModule,ConfigService} from "@nestjs/config";
+
+import { validate } from 'env.validation';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
     SongsModule,
     PlayListModule,
     AuthModule,
     UsersModule,
     ArtistsModule,
     SeedModule,
+    ConfigModule.forRoot({
+      envFilePath: [".env.development"],
+      isGlobal: true,
+      expandVariables: true,
+      // load: [configuration],
+      validationSchema: envSchema,
+    }), 
+
+        // Database connection
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            type: "postgres",
+            host: configService.get("DATABASE_HOST"),
+            port: parseInt(configService.get("DATABASE_PORT"), 10),
+            username: configService.get("DATABASE_USERNAME"),
+            password: configService.get("DATABASE_PASSWORD"),
+            database: configService.get("DATABASE_NAME"),
+            entities: [User,Song,Playlist,Artist],
+            synchronize: false,
+          }),
+          inject: [ConfigService]
+        }),
+        // TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  constructor(/*private dataSource: DataSource*/) {
-    // console.log('dbName ', dataSource.driver.database);
-  }
-  configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(LoggerMiddleware).forRoutes('songs'); // option no 1
-    // consumer
-    //   .apply(LoggerMiddleware)
-    //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); //option no 2
+export class AppModule {
+  // constructor(/*private dataSource: DataSource*/) {
+  //   // console.log('dbName ', dataSource.driver.database);
+  // }
+  // configure(consumer: MiddlewareConsumer) {
+  //   // consumer.apply(LoggerMiddleware).forRoutes('songs'); // option no 1
+  //   // consumer
+  //   //   .apply(LoggerMiddleware)
+  //   //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); //option no 2
 
-    consumer.apply(LoggerMiddleware).forRoutes(SongsController); //option no 3
-  }
+  //   consumer.apply(LoggerMiddleware).forRoutes(SongsController); //option no 3
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { SeedService } from './seed/seed.service';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  /**
-   * You can enable the seeding here
-   */
-  // const seedService = app.get(SeedService);
-  // await seedService.seed();
-  await app.listen(3000);
+
+      const app = await NestFactory.create(AppModule, { cors: { credentials: true} });
+
+       app.use(bodyParser.json({ limit: '50mb' }));
+       app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+      //Swagger connection
+       const config = new DocumentBuilder()
+      .setTitle('Spotify')
+      .setDescription('Spotify-clone backend')
+      .setVersion('1.0')
+      .addTag('Spotify')
+      .addBearerAuth({ in: 'header', type: 'http' })
+      .build();
+       const document = SwaggerModule.createDocument(app, config);
+       SwaggerModule.setup('api', app, document);
+
+     // Server Connection
+      const port = process.env.PORT ;
+      await app.listen(port, async () => {
+      console.log(`The server is running on ${port} port: http://localhost:${port}/api`);
+  });
 }
 bootstrap();
